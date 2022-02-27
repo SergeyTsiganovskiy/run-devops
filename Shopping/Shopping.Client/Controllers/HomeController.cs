c2d3726787bddbbbf11bc10ea1d4de2c677f5c10
Serhii Tsyhanivskyi <Serhii_Tsyhanivskyi@epam.com>
2022-02-27 17:52:00 +0200

    buildcontex add

--- Shopping/Shopping.Client/Controllers/HomeController.cs
+++ Shopping/Shopping.Client/Controllers/HomeController.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
-using Shopping.Client.Data;
 using Shopping.Client.Models;
 using System;
 using System.Collections.Generic;
@@ -23,14 +22,13 @@ namespace Shopping.Client.Controllers
             _httpClient = httpClientFactory.CreateClient("ShoppingAPIClient");
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            //var response = await _httpClient.GetAsync("/product");
-            //var content = await response.Content.ReadAsStringAsync();
-            //var productList = JsonConvert.DeserializeObject<IEnumerable<Product>>(content);
+            var response = await _httpClient.GetAsync("/product");
+            var content = await response.Content.ReadAsStringAsync();
+            var productList = JsonConvert.DeserializeObject<IEnumerable<Product>>(content);
 
-            //return View(productList);
-            return View(ProductContext.Products);
+            return View(productList);
         }
 
         public IActionResult Privacy()
